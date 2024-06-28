e8e38391c1f63e4dbaa2ac6fc46018873172cbfa
Adson <adson23_@hotmail.com>
2024-06-28 17:02:50 -0300

    Add: script to upload file to the bucket

--- create-s3-bucket.py
+++ create-s3-bucket.py
@@ -17,13 +17,38 @@ def create_s3_bucket(bucket_name, region=None):
         
         print(f'Bucket {bucket_name} was created in the region {region}!')
     
-    except (NoCredentialsError, PartialCredentialsError) as e:
-        print(f'Credentials error: {e}')
-    except Exception as e:
-        print(f'Error creating bucket: {e}')
+    except (NoCredentialsError, PartialCredentialsError) as err:
+        print(f'Credentials error: {err}')
+    except Exception as err:
+        print(f'Error creating bucket: {err}')
+        
+
+def upload_file_to_s3(bucket_name, file_name, object_name):
+    try:
+        session = boto3.Session()
+        
+        s3_client = session.client('s3')
+        
+        if object_name is None:
+            object_name = file_name.split('/')[-1]
+    
+        s3_client.upload_file(file_name, bucket_name, object_name)
+        
+        print(f'The file {file_name} was sent to bucket {bucket_name} with the name {object_name}.')
+    
+    except (NoCredentialsError, PartialCredentialsError) as err:
+        print(f'Credentials error: {err}')
+    except Exception as err:
+        print(f'Error uploading file: {err}')
 
 if __name__ == "__main__":
     bucket_name = 'my-test-bucket.v1'
     region = 'us-east-1'
     
     create_s3_bucket(bucket_name, region)
+
+    file_name = '/mnt/c/users/adson/OneDrive/Documentos/AWS/S3-bucket/setup.jpg'
+    object_name = 'setup'
+    
+    upload_file_to_s3(bucket_name, file_name, object_name)
+    
